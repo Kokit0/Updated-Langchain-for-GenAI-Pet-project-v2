ecc3919993f7f28728522f8cf6af800376ae97af
Kokke <jorge.amaya.s@gmail.com>
2024-08-18 22:04:32 -0400

    Actualización app.py prueba con OpenAI api key

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,41 +1,42 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import (
+    StrOutputParser,
+)  # parser del output por defecto (pero luego usaré uno custom)
 
 import streamlit as st
 import os
 from dotenv import load_dotenv
 
-os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
-## Langmith tracking
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+## Langsmith tracking (super util para traquear lo que hagamos)
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
-## Prompt Template
+## Prompt Template (chatbot prompt template for chat duh)
 
-prompt=ChatPromptTemplate.from_messages(
+prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpful assistant. Please response to the user queries"),
-        ("user","Question:{question}")
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question:{question}"),
     ]
 )
 
 ## streamlit framework
 
-st.title('Langchain Demo Con OPENAI API')
-input_text=st.text_input("Search the topic u want")
+st.title("Langchain Demo Con OPENAI API")
+input_text = st.text_input("Busca sobre el tópico que tu quieras")
 
-# openAI LLm
-llm=ChatOpenAI(model="gpt-3.5-turbo")
-output_parser=StrOutputParser()
-chain=prompt|llm|output_parser
+# OpenAI LLM
+llm = ChatOpenAI(model="gpt-3.5-turbo") #integramos ocn el llm
+output_parser = StrOutputParser() # con output parser obtenemos el resultado "trozado"
+chain = prompt | llm | output_parser # combinamos encadenado, las 3 acciones
 
 if input_text:
-    st.write(chain.invoke({'question':input_text}))
+    st.write(chain.invoke({"question": input_text}))
 
 
-### Claves y api keys de este proyecto en el .env
-#   LANGCHAIN_API_KEY=""
-#   OPENAI_API_KEY=""
-#   LANGCHAIN_KEY="Tutorial1"
-###
+
+"""
+Desarrollado por Jorge Amaya S. 2024
+"""
